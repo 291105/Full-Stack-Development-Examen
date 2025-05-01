24d0e053b490ae900742ed26d0c632282c5aaa57
Sander <sander.delanghe@student.vives.be>
2025-05-02 00:25:51 +0200

    domain

---
+++ Flight.Domaon/Entities/HotelSearchResult.cs
@@ -0,0 +1,11 @@
+﻿namespace FlightProject.Domain.Entities
+{
+    public class HotelSearchResult
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public double StarRating { get; set; }
+        public string HotelUrl { get; set; } = string.Empty;
+
+    }
+}
--- Flight.Domaon/obj/Debug/net8.0/FlightProject.Domain.AssemblyInfo.cs
+++ Flight.Domaon/obj/Debug/net8.0/FlightProject.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlightProject.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+279085b978088da89b1a1d36b18db6b6e28f1424")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlightProject.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlightProject.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
