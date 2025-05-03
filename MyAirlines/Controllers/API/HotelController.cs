8917053702b9ba51317dcc24822fbb35ac613ed4
Sander <sander.delanghe@student.vives.be>
2025-05-03 11:37:09 +0200

    + HotelController

---
+++ MyAirlines/Controllers/API/HotelController.cs
@@ -0,0 +1,28 @@
+﻿using FlightProject.Domain.Data;
+using FlightProject.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using MyAirlines.ViewModels;
+
+namespace MyAirlines.Api.Controllers
+{
+    public class HotelController : Controller
+    {
+        private readonly IHotelsApiService _hotelsService;
+
+        public HotelController(IHotelsApiService hotelsService)
+        {
+            _hotelsService = hotelsService;
+        }
+
+        public async Task<IActionResult> Index(string city = "")
+        {
+            List<Hotel> results = new();
+            if (!string.IsNullOrWhiteSpace(city))
+            {
+                results = await _hotelsService.GetHotelsAsync(city);
+            }
+            ViewData["City"] = city;
+            return View(results);
+        }
+    }
+}
