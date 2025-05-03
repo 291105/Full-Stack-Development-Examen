ba25f1672094f85e6517d99d8897b0b55e3d2d5f
Sander <sander.delanghe@student.vives.be>
2025-05-03 11:38:50 +0200

    + HotelSearchVM

---
+++ MyAirlines/ViewModels/HotelSearchResultVM.cs
@@ -0,0 +1,11 @@
+﻿namespace MyAirlines.ViewModels
+{
+    public class HotelSearchResultVM
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public double StarRating { get; set; }
+        public string HotelUrl { get; set; } = string.Empty;
+
+    }
+}
