bf06af3d82fe992bfc9cdad5d03cca4d6c25cb07
Sander <sander.delanghe@student.vives.be>
2025-05-03 11:36:12 +0200

    + IHotelsApiService

---
+++ Flight.Services/Interfaces/IHotelsApiService.cs
@@ -0,0 +1,9 @@
+﻿using FlightProject.Domain.Data;
+
+namespace FlightProject.Services.Interfaces
+{
+    public interface IHotelsApiService
+    {
+        public Task<List<Hotel>> GetHotelsAsync(string city);
+    }
+}
