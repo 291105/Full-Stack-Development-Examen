54019abe75074a755b49cdca5481ddc8612a1df0
Sander <sander.delanghe@student.vives.be>
2025-05-04 13:43:55 +0200

    Add files via upload

---
+++ MyAirlines/ViewModels/SendMailVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SendMail.ViewModels
+{
+    public class SendMailVM
+    {
+        [Required, Display(Name = "Jouw naam")]
+        public string? FromName { get; set; }
+        [Required, Display(Name = "Jouw e-mailadres"), EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string? Message { get; set; }
+        public bool Invoice { get; set; }
+    }
+}
